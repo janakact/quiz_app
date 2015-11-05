144ad6029d3de1d74c662759460f95c5a9e800d6
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-11-06 00:22:58 +0530

    courses done-S

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic.service.platform', 'ionic.ui.content', 'ionic.ui.list'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, socket) {
 
@@ -221,4 +221,56 @@ angular.module('starter.controllers', [])
 
 
     return myIoSocket;
+  })
+
+
+  // A simple relative timestamp filter
+  .filter('relativets', function() {
+    return function(value) {
+      var now = new Date();
+      var diff = now - value;
+
+      // ms units
+      var second = 1000;
+      var minute = second * 60;
+      var hour = minute * 60;
+      var day = hour * 24;
+      var year =  day * 365;
+      var month = day * 30;
+
+      var unit = day;
+      var unitStr = 'd';
+      if(diff > year) {
+        unit = year;
+        unitStr = 'y';
+      } else if(diff > day) {
+        unit = day;
+        unitStr = 'd';
+      } else if(diff > hour) {
+        unit = hour;
+        unitStr = 'h';
+      } else if(diff > minute) {
+        unit = minute;
+        unitStr = 'm';
+      } else {
+        unit = second;
+        unitStr = 's';
+      }
+
+      var amt = Math.ceil(diff / unit);
+      return amt + '' + unitStr;
+    }
+  })
+
+  .controller('ProfileCtrl', function($scope) {
+    $scope.posts = [];
+
+    for(var i = 0; i < 7; i++) {
+      // Fake a date
+      var date = (+new Date) - (i * 1000 * 60 * 60);
+      $scope.posts.push({
+        created_at: date,
+        text: 'Doing a bit of ' + ((Math.floor(Math.random() * 2) === 1) ? 'that' : 'this')
+      });
+    }
   });
